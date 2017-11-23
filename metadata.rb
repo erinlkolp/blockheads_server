a28a44d12d429907f93634cb866900ff819847b8
erinlkolp <erinlkolpfoss@gmail.com>
2017-11-22 18:26:14 -0700

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,7 @@ maintainer_email 'erinlkolpfoss@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures blockheads_server'
 long_description 'Installs/Configures blockheads_server'
-version '0.1.0'
+version '17.11.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/blockheads-server/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/blockheads-server'
+issues_url 'https://github.com/erinlkolp/blockheads_server/issues'
+source_url 'https://github.com/erinlkolp/blockheads_server'
