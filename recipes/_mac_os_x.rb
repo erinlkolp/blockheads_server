fbf1b72f1a7c45ce816ec5c35571cbeae486c50f
erinlkolp <erinlkolpfoss@gmail.com>
2017-11-22 18:23:27 -0700

    Fix name of unzip resource call.

--- recipes/_mac_os_x.rb
+++ recipes/_mac_os_x.rb
@@ -8,10 +8,10 @@ CHECKSUM = '170eb3a5489ea7b3a9332fbaf444e143ab0eb2423a381ce668948ca33a6c4a11'
 remote_file "#{Chef::Config[:file_cache_path]}/BlockheadsServer.zip" do
   source "http://theblockheads.net/share/BlockheadsServer.zip"
   checksum "#{CHECKSUM}"
-  notifies :run, 'execute[unzip-hipchat]'
+  notifies :run, 'execute[unzip-bserver]'
 end
 
-execute 'unzip-hipchat' do
+execute 'unzip-bserver' do
   command "unzip #{Chef::Config[:file_cache_path]}/BlockheadsServer.zip -d /Applications"
   action :nothing
 end
